231ef9951d1c0bc706e661fce3a9f3fe93388a70
unknown <nacholop@gmail.com>
2018-04-27 14:03:20 +0200

    actualizado 27 de abril

---
+++ arriba.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
